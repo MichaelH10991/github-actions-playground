4551e1c51dba2429e3c3bf86fbc334f0c18783a9
Michael Humphries <michaelhumphries1997@gmail.com>
2025-05-28 22:40:58 +0100

    some change

---
+++ .changeset/giant-moose-grin.md
@@ -0,0 +1,5 @@
+---
+"github-actions-playground": major
+---
+
+some change
---
+++ scripts/tag.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+version="v$(node -p \"require('./package.json').version\")"
+
+git tag $version --force && git push --tags --force
+
+exit 0
